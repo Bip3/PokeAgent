0663117b3f06268e60f0279c49abe4e57176d281
Natalie King <kingandrew739@gmail.com>
2025-12-09 19:26:34 -0500

    new training scenario

---
+++ run_parallel_aggro.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#$ -l h_rt=12:00:00
+#$ -pe omp 4
+#$ -N poke_parallel_aggro
+#$ -o logs/poke_parallel_aggro.out
+#$ -e logs/poke_parallel_aggro.err
+#$ -cwd
+
+mkdir -p logs params
+
+# Limit threading for linear algebra libraries
+export OMP_NUM_THREADS=4
+export OPENBLAS_NUM_THREADS=4
+export MKL_NUM_THREADS=4
+export VECLIB_MAXIMUM_THREADS=4
+export NUMEXPR_NUM_THREADS=4
+
+javac -cp "./lib/*:." @pokePA.srcs
+
+java -XX:ActiveProcessorCount=4 -XX:ParallelGCThreads=2 -cp "./lib/*:." edu.bu.pas.pokemon.ParallelTrain \
+    edu.bu.pas.pokemon.agents.AggroAgent \
+    -p 2000 \
+    -t 500 \
+    -v 200 \
+    -b 15000 \
+    -r RANDOM \
+    -u 2 \
+    -m 128 \
+    -n 1e-5 \
+    -c 10 \
+    -d adam \
+    -g 0.99 \
+    -o params/easy_updated_qFunc \
+    --seed 123 \
+    -j 4 \
+    > logs/poke_parallel_aggro.log
